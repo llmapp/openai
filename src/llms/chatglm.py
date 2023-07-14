92500bfe76a9b1122abf42a6e53b77874631add2
Huajian Mao <huajianmao@gmail.com>
2023-07-14 15:40:24 +0800

    fix: support chatglm-6b

--- src/llms/chatglm.py
+++ src/llms/chatglm.py
@@ -7,8 +7,10 @@ from ..type import ChatMessage
 
 def load_model(model_id: str):
     tokenizer = AutoTokenizer.from_pretrained(model_id, trust_remote_code=True)
-    model = AutoModel.from_pretrained(model_id, trust_remote_code=True).cuda()
-    model.eval()
+    model = AutoModel.from_pretrained(model_id, trust_remote_code=True)
+    if model_id == 'THUDM/chatglm-6b':
+        model.half()
+    model.cuda().eval()
 
     return model, tokenizer
 
