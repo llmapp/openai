281caf9e1cbb620a3581186cbce7c0a265b363dd
Huajian Mao <huajianmao@gmail.com>
2023-07-21 21:38:06 +0800

    chore: improve llama chat and stream_chat

--- src/llms/llama.py
+++ src/llms/llama.py
@@ -3,7 +3,6 @@ import torch
 from dotenv import load_dotenv
 from threading import Thread
 from transformers import AutoModelForCausalLM, AutoTokenizer, TextIteratorStreamer
-from transformers.generation.utils import GenerationConfig
 from typing import List
 
 from ..type import ChatMessage
@@ -15,38 +14,39 @@ WEIGHTS_PATH = os.environ.get("LLAMA_WEIGHTS")
 
 def _load_model(model_name: str):
     path = os.path.join(WEIGHTS_PATH, model_name)
-    tokenizer = AutoTokenizer.from_pretrained(path, use_fast=False, trust_remote_code=True)
-    model = AutoModelForCausalLM.from_pretrained(path, device_map="cuda", trust_remote_code=True).eval()
-    model.generation_config = GenerationConfig.from_pretrained(path)
+
+    model = AutoModelForCausalLM.from_pretrained(path, device_map="cuda").eval()
+
+    tokenizer = AutoTokenizer.from_pretrained(path, use_fast=False)
+    tokenizer.add_special_tokens({"pad_token": "<PAD>"})
     return model, tokenizer
 
 
+default_gen_kwargs = {"do_sample": True, "max_length": 2048,
+                      "temperature": 1.0, "repetition_penalty": 1.2, "top_p": 0.95}
+
+
 def _chat(model, tokenizer, messages: List[ChatMessage]):
-    ctx = _messages_to_ctx(messages)
-    ctx += "\nassistant: "
-    input_ids = tokenizer.encode(ctx, return_tensors="pt")
-    response = model.generate(input_ids.cuda(),
-                              do_sample=True,
-                              max_length=2048,
-                              temperature=1.0,
-                              repetition_penalty=1.2,
-                              eos_token_id=tokenizer.eos_token_id)
-    generated = tokenizer.decode(response[0])
-    text = generated[len(ctx) + len('<s>'):].strip()
-    text = text[:text.find('</s>')].strip()
-    return text, None
+    chat = format_tokens(messages, tokenizer)
+    input_ids = torch.tensor(chat).long()
+    input_ids = input_ids.unsqueeze(0)
+    input_ids = input_ids.to("cuda")
+
+    response = model.generate(input_ids, **default_gen_kwargs, eos_token_id=tokenizer.eos_token_id)
+    generated = tokenizer.decode(response[0], skip_prompt=True, skip_special_tokens=True)
+    generated = generated[generated.find("[/INST]") + 8:].strip()
+    return generated, None
 
 
 def _stream_chat(model, tokenizer, messages: List[ChatMessage]):
-    gen_kwargs = {"do_sample": True, "max_length": 2048, "temperature": 1.0,
-                  "repetition_penalty": 1.2, "top_p": 0.95, "eos_token_id": tokenizer.eos_token_id}
-
-    ctx = _messages_to_ctx(messages)
-    ctx += "\nassistant: "
-    input_ids = tokenizer.encode(ctx, return_tensors="pt")
+    gen_kwargs = {**default_gen_kwargs, "eos_token_id": tokenizer.eos_token_id}
+    chat = format_tokens(messages, tokenizer)
+    input_ids = torch.tensor(chat).long()
+    input_ids = input_ids.unsqueeze(0)
+    input_ids = input_ids.to("cuda")
     gen_kwargs["input_ids"] = input_ids.cuda()
 
-    streamer = TextIteratorStreamer(tokenizer, timeout=60.0, skip_prompt=True)
+    streamer = TextIteratorStreamer(tokenizer, timeout=60.0, skip_prompt=True, skip_special_tokens=True)
     gen_kwargs["streamer"] = streamer
 
     thread = Thread(target=model.generate, kwargs=gen_kwargs)
@@ -58,16 +58,43 @@ def _stream_chat(model, tokenizer, messages: List[ChatMessage]):
 HANDLERS = {"load": _load_model, "chat": _chat, "stream_chat": _stream_chat}
 
 
-def _messages_to_ctx(messages: List[ChatMessage]):
-    ctx = """"\
+Dialog = List[ChatMessage]
+
+B_INST, E_INST = "[INST]", "[/INST]"
+B_SYS, E_SYS = "<<SYS>>\n", "\n<</SYS>>\n\n"
+DEFAULT_SYSTEM_PROMPT = """\
 You are a helpful, respectful and honest assistant. Always answer as helpfully as possible, while being safe.  Your answers should not include any harmful, unethical, racist, sexist, toxic, dangerous, or illegal content. Please ensure that your responses are socially unbiased and positive in nature.
 
 If a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. If you don't know the answer to a question, please don't share false information."""
-    for message in messages:
-        ctx += "\n" + _message_to_ctx(message)
-    ctx = (ctx[-1920:]) if len(ctx) >= 2048 else ctx
-    return ctx
 
 
-def _message_to_ctx(message: ChatMessage):
-    return message.role + ": " + message.content
+def format_tokens(dialog, tokenizer):
+    if dialog[0].role != "system":
+        dialog = [ChatMessage(role="system", content=DEFAULT_SYSTEM_PROMPT)] + dialog
+    dialog = [ChatMessage(role=dialog[1].role, content=B_SYS
+                          + dialog[0].content
+                          + E_SYS
+                          + dialog[1].content)] + dialog[2:]
+    assert all([msg.role == "user" for msg in dialog[::2]]) and all(
+        [msg.role == "assistant" for msg in dialog[1::2]]
+    ), (
+        "model only supports 'system','user' and 'assistant' roles, "
+        "starting with user and alternating (u/a/u/a/u...)"
+    )
+    dialog_tokens: List[int] = sum(
+        [
+            tokenizer.encode(
+                f"{B_INST} {(prompt.content).strip()} {E_INST} {(answer.content).strip()} ",
+            )
+            for prompt, answer in zip(dialog[::2], dialog[1::2])
+        ],
+        [],
+    )
+    assert (
+        dialog[-1].role == "user"
+    ), f"Last message must be from user, got {dialog[-1].role}"
+    dialog_tokens += tokenizer.encode(
+        f"{B_INST} {(dialog[-1].content).strip()} {E_INST}",
+    )
+
+    return dialog_tokens
